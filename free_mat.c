2b391af7c9c828620c68e60bb0cc7cf150eac2f1
Seamus Mulholland-Patterson <seamus1103@gmail.com>
2019-09-22 16:11:10 +0800

    added trace for csc

--- free_mat.c
+++ free_mat.c
@@ -3,3 +3,16 @@
 void free_coo(coo matrix){
 	free(matrix.elems);
 }
+
+void free_csr(csr matrix){
+	if(matrix.type == MAT_INT)
+		free(matrix.nnz.i);
+	else
+		free(matrix.nnz.f);
+	free(matrix.ia);
+	free(matrix.ja);
+}
+
+void free_csc(csc matrix){
+	free_csr(matrix);
+}
